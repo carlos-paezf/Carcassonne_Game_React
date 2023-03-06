82bc01cfad12f503fe57e6a8e6a3f839f5b43650
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-06 18:19:00 -0500

    Add Toast Context

--- src/app/context/index.ts
+++ src/app/context/index.ts
@@ -1,19 +1,35 @@
 import { createContext } from "react";
-import { GameInfoType } from "../types";
+import { GameInfoType, ToastPropsType } from "../types";
 
 
-type ContextType = {
+type GameInfoContextType = {
     gameInfo: GameInfoType;
     setGameInfo: React.Dispatch<React.SetStateAction<GameInfoType>>;
 };
 
 
-export const GameInfoContext = createContext<ContextType>( {
+export const GameInfoContext = createContext<GameInfoContextType>( {
     gameInfo: {
         tilesInDeck: 0,
         turn: 0,
         numberDiscards: 0,
-        score: 0
+        score: 0,
+        settingsGame: {
+            playerName: '',
+            boardSize: 0
+        }
     },
     setGameInfo: () => { }
+} );
+
+
+type ToastContextType = {
+    toasts: ToastPropsType[],
+    setToasts: React.Dispatch<React.SetStateAction<ToastPropsType[]>>;
+};
+
+
+export const ToastContext = createContext<ToastContextType>( {
+    toasts: [],
+    setToasts: () => { }
 } );
