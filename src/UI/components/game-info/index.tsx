0cf99e33f003c8f0ae0f6cca25d520910dba1653
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-05 20:16:10 -0500

    Add Game Info Component

---
+++ src/UI/components/game-info/index.tsx
@@ -0,0 +1,27 @@
+import { FC, useContext } from "react";
+import { GameInfoContext } from '../../../app/context/index';
+
+
+export const GameInfo: FC = () => {
+    const { gameInfo } = useContext( GameInfoContext );
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Tiles in Deck</th>
+                    <th>Actual Turn</th>
+                    <th>Number of discards remaining</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <td>{ gameInfo.tilesInDeck }</td>
+                <td>{ gameInfo.turn }</td>
+                <td>{ gameInfo.numberDiscards }</td>
+                <td>{ gameInfo.score }</td>
+            </tbody>
+        </table>
+    );
+};
