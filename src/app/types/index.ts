afc71f1d5daac704e304469f779bdc4efe7e01ac
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-06 18:19:10 -0500

    Update Types

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -3,10 +3,17 @@ export type GameInfoType = {
     turn: number;
     numberDiscards: number;
     score: number;
+    settingsGame: SettingsGameType;
 };
 
 
 export type SettingsGameType = {
     playerName: string;
     boardSize: number;
+};
+
+
+export type ToastPropsType = {
+    message: string;
+    type?: string;
 };
