3b49947c76ca584f9987255f68a37f2d2fc9fcfb
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-05 20:15:11 -0500

    Add Board Component

---
+++ src/UI/components/board/Board.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+
+export const Board: FC = ( /* size: number */ ) => {
+    return (
+        <table>
+            <tbody>
+            </tbody>
+        </table>
+    );
+};
