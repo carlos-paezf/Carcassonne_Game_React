cc4a5a4e473d254b072a480826b4fa4cc599d875
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-07 18:37:41 -0500

    Add Constants

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -8,6 +8,47 @@ export const initialGameInfo: IGameInfo = {
     score: 0,
     settingsGame: {
         playerName: '',
-        boardSize: 3
+        boardSize: 11
     }
 };
+
+
+export const TILES_PER_HAND = 4;
+
+
+export enum TileType {
+    ROAD = 'ROAD',
+    CITY = 'CITY',
+    ABBEY = 'ABBEY'
+};
+
+
+export const tileInterval = {
+    road: ( 9 / 15 ),
+    city: ( 5 / 15 ),
+    abbey: ( 1 / 15 )
+};
+
+
+export enum TilePoint {
+    ROAD = 1,
+    ABBEY = 1,
+    CITY = 2,
+    CHAIN = 1
+};
+
+
+export enum RoadDirection {
+    RIGHT_LEFT = 'RIGHT_LEFT',
+    UP_DOWN = 'UP_DOWN',
+    FOUR_WAY = 'FOUR_WAY',
+    THREE_WAY = 'THREE_WAY',
+    CORNER = 'CORNER'
+};
+
+
+export enum IncrementType {
+    ABBEY_NEIGHBORHOOD = 'ABBEY_NEIGHBORHOOD',
+    BY_CITY_CHAIN = 'BY_CITY_CHAIN',
+    BY_NEIGHBORING_ABBEY = 'BY_NEIGHBORING_ABBEY'
+}
