763706de23d9fb1f8e38ab4c514925ea462c5bab
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-08 11:24:54 -0500

    Update useMultipleContext hook

--- src/app/hooks/useMultipleContext.tsx
+++ src/app/hooks/useMultipleContext.tsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import { RoadDirection, TileType, initialGameInfo } from "../constants";
 import { Tile } from "../game-logic/tile";
 import { IGameInfo, IToastProps } from "../types";
+import { useGameContext } from "./useGameContext";
+import { useToastContext } from "./useToastContext";
+import { useBoardContext } from "./useBoardContext";
+import { useHandContext } from "./useHandContext";
 
 
 /**
@@ -10,59 +14,15 @@ import { IGameInfo, IToastProps } from "../types";
  * @returns An object with three properties: gameContext, toastContext, and boardContext.
  */
 export const useMultipleContext = () => {
-    const [ gameInfo, setGameInfo ] = useState<IGameInfo>( initialGameInfo );
-    const [ toasts, setToasts ] = useState<IToastProps[]>( [] );
-    const [ board, setBoard ] = useState<( Tile | null )[][]>( [] );
-
-
-    /**
-     * AddToast is a function that takes an object with a message and a type property, and adds it to
-     * the toasts array.
-     * @param {IToastProps}  - IToastProps - This is the type of the parameter that is being passed in.
-     */
-    const addToast = ( { message, type }: IToastProps ) => {
-        setToasts( [ ...toasts, { message, type } ] );
-    };
-
-
-    /**
-     * Create a board of size x size, and set the middle tile to a road.
-     * @param {number} size - number - The size of the board.
-     */
-    const createBoard = ( size: number ) => {
-        const board: ( Tile | null )[][] = [];
-
-        for ( let i = 0; i < size; i++ ) {
-            const row = [];
-            for ( let j = 0; j < size; j++ ) {
-                row.push( null );
-            }
-            board.push( row );
-        }
-
-        const middle = Math.floor( size / 2 );
-        board[ middle ][ middle ] = new Tile( TileType.ROAD, RoadDirection.FOUR_WAY );
-
-        setBoard( board );
-    };
-
-
-    /**
-     * UpdateBoard is a function that takes a tile, row, and column, and returns a new board with the
-     * tile placed at the row and column.
-     * @param {Tile} tile - Tile - this is the tile that is being placed on the board
-     * @param {number} row - number - The row of the tile that was clicked
-     * @param {number} col - number - the column number of the tile that was clicked
-     */
-    const updateBoard = ( tile: Tile, row: number, col: number ) => {
-        const newBoard = [ ...board ];
-        newBoard[ row ][ col ] = tile;
-        setBoard( newBoard );
-    };
+    const { gameInfo, setGameInfo } = useGameContext();
+    const { toasts, addToast } = useToastContext();
+    const { board, createBoard, updateBoard } = useBoardContext();
+    const { hand, appendTileToHand, discardHand } = useHandContext();
 
     return {
         gameContext: { gameInfo, setGameInfo },
         toastContext: { toasts, addToast },
-        boardContext: { board, createBoard, updateBoard }
+        boardContext: { board, createBoard, updateBoard },
+        handContext: { hand, appendTileToHand, discardHand }
     };
 };
