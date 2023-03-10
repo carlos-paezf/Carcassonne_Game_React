6ce2895f4d0ecd64e7baac70f4a086192e783ee7
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-09 21:02:06 -0500

    Update Hand Component

--- src/app/UI/components/hand/index.tsx
+++ src/app/UI/components/hand/index.tsx
@@ -1,22 +1,89 @@
-import { FC, useContext, useEffect, useState } from "react";
-import { HandContext } from "../../../context";
+import { FC, useEffect } from "react";
+import { useGameInfo, useGameInfoDispatch } from "../../../reducer/game-reducer";
+import { useHand, useHandDispatch } from "../../../reducer/hand-reducer";
+import { useToastDispatch } from "../../../reducer/toast-reducer";
 import { TileComponent } from "../tile/tile";
+import { TILES_PER_HAND, ToastType } from "../../../constants";
 
 
 export const Hand: FC = () => {
-    const { hand, discardHand } = useContext( HandContext );
+    // const { hand, discardHand } = useContext( HandContext );
+    const hand = useHand();
+    const dispatchHand = useHandDispatch();
+    const dispatchToast = useToastDispatch();
+    const { numberDiscards, tilesInDeck, turn } = useGameInfo();
+    const dispatchGameInfo = useGameInfoDispatch();
 
-    // useEffect( () => {
-    // }, [ hand ] );
+
+    const handleDiscardHand = () => {
+        if ( numberDiscards === 0 ) {
+            return dispatchToast( {
+                type: 'addToast',
+                payload: {
+                    message: 'You cannot discard your current hand. You already used the maximum amount of discards',
+                    type: ToastType.ERROR
+                }
+            } );
+        }
+
+        if ( tilesInDeck! < TILES_PER_HAND ) {
+            return dispatchToast( {
+                type: 'addToast',
+                payload: {
+                    message: 'You cannot discard your current hand. There are not enough cards to distribute a new hand.',
+                    type: ToastType.ERROR
+                }
+            } );
+        }
+
+        dispatchHand( {
+            type: 'discardHand',
+            payload: {
+                numberDiscards,
+                tilesInDeck
+            }
+        } );
+
+        dispatchGameInfo( {
+            type: 'updateTilesInDeck',
+            payload: {
+                tilesInDeck: tilesInDeck - TILES_PER_HAND
+            }
+        } );
+
+        dispatchGameInfo( {
+            type: 'updateTurn',
+            payload: {
+                turn: turn + 1
+            }
+        } );
+
+        dispatchGameInfo( {
+            type: 'updateNumberOfDiscards',
+            payload: {
+                numberDiscards: numberDiscards - 1
+            }
+        } );
+    };
+
+    useEffect( () => {
+        dispatchHand( {
+            type: 'initialHand',
+            payload: {
+                numberDiscards,
+                tilesInDeck
+            }
+        } );
+    }, [] );
 
     return (
         <div className="hand">
             <div className="tiles">
                 {
-                    hand.map( ( tile, idx ) => <TileComponent key={ idx } tile={ tile } index={ idx } /> )
+                    hand.map( tile => <TileComponent key={ tile.id } tile={ tile.tile } index={ tile.id } /> )
                 }
             </div>
-            <button onClick={ discardHand }>Discard Hand</button>
+            <button onClick={ handleDiscardHand }>Discard Hand</button>
         </div>
     );
 };
