1375394672ed451d376ea7b4a6b7498e339f3daa
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-10 20:01:54 -0500

    Update General Provider

--- src/app/reducer/general-provider.tsx
+++ src/app/reducer/general-provider.tsx
@@ -6,7 +6,12 @@ import { BoardProvider } from './board-reducer';
 import { HandProvider } from "./hand-reducer";
 
 
-
+/**
+ * The GeneralProvider is a function that returns a ToastProvider that wrap a GameProvider that
+ * wrap a HandProvider that wrap a BoardProvider that wrap the children.
+ * @param  - FC<Props> = ( { children } ) => {
+ * @returns The children of the GeneralProvider component.
+ */
 export const GeneralProvider: FC<Props> = ( { children } ) => {
     return (
         <ToastProvider>
