ba2edad004c15df3b00f1d800acf07ac4b34d65b
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-10 20:04:24 -0500

    Remove Context Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,22 @@ import { GameInfo } from './app/UI/components/game-info/index';
 import { Hand } from './app/UI/components/hand';
 import { Header } from './app/UI/components/header/index';
 import { ToastContainer } from './app/UI/components/toast/index';
-import { MultipleContextProvider } from './app/context/context-provider';
 import { GeneralProvider } from './app/reducer/general-provider';
 
 
 function App () {
     return (
-        <MultipleContextProvider>
-            <GeneralProvider>
-                <div className="App">
-                    <Header />
-                    <GameInfo />
+        <GeneralProvider>
+            <div className="App">
+                <Header />
+                <GameInfo />
 
-                    <Hand />
-                    <Board />
+                <Hand />
+                <Board />
 
-                    <ToastContainer />
-                </div>
-            </GeneralProvider>
-        </MultipleContextProvider>
+                <ToastContainer />
+            </div>
+        </GeneralProvider>
     );
 }
 
