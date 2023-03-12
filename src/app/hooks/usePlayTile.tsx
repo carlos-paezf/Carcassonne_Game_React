7c667d1c9ca72751ec85ad1055a79e61474d546b
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-11 19:25:56 -0500

    Add usePlayTile hook

---
+++ src/app/hooks/usePlayTile.tsx
@@ -0,0 +1,55 @@
+import { Tile } from "../game-logic/tile";
+import { useBoardDispatch } from "../reducer/board-reducer";
+import { useGameInfo, useGameInfoDispatch } from "../reducer/game-reducer";
+import { useHandDispatch } from "../reducer/hand-reducer";
+
+
+/**
+ * PlayTile is a function that takes in an indexTile, a tile, a row, and a col, and then dispatches a
+ * playTile action to the board, hand, and gameInfo reducers, and then decrements the tilesInDeck and
+ * increments the turn.
+ * 
+ * @returns The playTile function is being returned.
+ */
+export const usePlayTile = () => {
+    const dispatchBoard = useBoardDispatch();
+    const dispatchHand = useHandDispatch();
+    const gameInfoDispatch = useGameInfoDispatch();
+    const { tilesInDeck, turn } = useGameInfo();
+
+    /**
+     * PlayTile is a function that takes in an indexTile, a tile, a row, and a col, and then dispatches
+     * a playTile action to the board, hand, and gameInfo reducers, and then decrements the tilesInDeck
+     * and increments the turn.
+     * @param {number} indexTile - the index of the tile in the hand array
+     * @param {Tile} tile - Tile, row: number, col: number
+     * @param {number} row - number, col: number -&gt; the row and column of the tile that was clicked
+     * on the board.
+     * @param {number} col - number - the column of the tile on the board
+     */
+    const playTile = ( indexTile: number, tile: Tile, row: number, col: number ) => {
+        dispatchBoard( {
+            type: 'playTile',
+            payload: {
+                tile: { tile, row, col }
+            }
+        } );
+
+        dispatchHand( {
+            type: 'playTile',
+            payload: {
+                indexTile, tilesInDeck
+            }
+        } );
+
+        gameInfoDispatch( {
+            type: 'playTile',
+            payload: {
+                tilesInDeck: tilesInDeck - 1,
+                turn: turn + 1
+            }
+        } );
+    };
+
+    return { playTile };
+};
