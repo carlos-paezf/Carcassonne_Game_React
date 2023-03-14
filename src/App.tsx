51be5774d57bca7968aa7768fa328642a073b00a
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-03-13 19:32:45 -0500

    Update App Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,34 @@ import { useState } from 'react';
 import './App.css';
 
 import { Board } from './app/UI/components/Board';
+import { EndGame } from './app/UI/components/EndGame';
 import { FormTile } from './app/UI/components/FormTile';
 import { GameInfo } from './app/UI/components/GameInfo';
 import { Hand } from './app/UI/components/Hand';
 import { Header } from './app/UI/components/Header';
 import { ToastContainer } from './app/UI/components/Toasts';
 import { FormTileContext } from './app/context/form-tile-context';
-import { ITile } from './app/types';
 import { useGameInfo } from './app/reducer/game-reducer';
+import { ITile } from './app/types';
 
 
 function App () {
-    const [ isVisible, setIsVisible ] = useState( false );
+    const [ formTileIsVisible, setFormTileIsVisible ] = useState( false );
     const [ selectedTile, setSelectedTile ] = useState<ITile>();
     const { tilesInDeck, turn } = useGameInfo();
 
     return (
-        <FormTileContext.Provider value={ { isVisible, setIsVisible, selectedTile, setSelectedTile } }>
+        <FormTileContext.Provider value={ { isVisible: formTileIsVisible, setIsVisible: setFormTileIsVisible, selectedTile, setSelectedTile } }>
             <div className="App">
                 <Header />
                 <GameInfo />
-
                 {
                     !( tilesInDeck === 0 && turn === 0 ) && <>
                         <Hand />
                         <Board />
+                        <EndGame />
                     </>
                 }
-
                 <FormTile />
                 <ToastContainer />
             </div>
